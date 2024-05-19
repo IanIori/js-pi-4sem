e3faf4f9704bf3b56733dff55f8ae28da6f5dfe1
Lucas Hackmann <lucas.hackmann@hotmail.com>
2024-05-19 15:24:24 -0300

    adicionado componente e pagina cadastro

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {Input} from "@nextui-org/react";
 
 export default function InputI() {
@@ -6,9 +5,9 @@ export default function InputI() {
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
       <Input 
       isRequired
-      type="email" 
-      label="Email" />
-      <Input type="email" label="Email" placeholder="Enter your email" />
+      type="" 
+      label="" />
+      <Input type="" label="" placeholder="" />
     </div>
   );
 }
